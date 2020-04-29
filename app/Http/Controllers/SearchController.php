2ac86b250096b931b5de0e83bf6c0dfd70d26386
BPL.ForWORK <bpl.forwork@Pamarees-MacBook-Pro-3.local>
2020-04-30 02:19:47 +0700

    add filter in drugPage (ยังไม่ได้ทำตรงquery Name)

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class SearchController extends Controller
+{
+    public function index(){
+
+        if (!empty($_GET)){
+            $year = $_GET['year'];
+            $method = $_GET['method'];
+            $GT = $_GET['GT'];
+            $Dname= $_GET['Dname'];
+            // dump($_GET);
+            // echo "<p>Your drug name is <b>" . $Dname . "</b>.</p>";       
+        }
+        print($GT);
+        $statement = "select * from Gini_drugs_".$GT." where BUDGET_YEAR = ".$year." and Method = '".$method."';";
+        $resultSearch = DB::select($statement);
+        // print($statement);
+        // dump($resultSearch);
+        // print(count($resultSearch));
+        return view('DrugPage', [ 'resultSearch' => $resultSearch ] );
+    }
+}
